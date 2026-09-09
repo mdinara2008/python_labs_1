59f962577801452e2db65b9626858b90a6e70e31
mdinara2008 <muhamedova2008@mail.ru>
2026-09-09 15:18:52 +0300

    1

--- src/lab01/01_greeting.py
+++ src/lab01/01_greeting.py
@@ -1,3 +1,4 @@
-name = input()
-age = int(input())
+name = input('Имя: ')
+age = int(input('Возраст: '))
 print('Привет,', name, '!', 'Через год тебе будет', age+1, '.')
+
--- src/lab01/02_sum_avg.py
+++ src/lab01/02_sum_avg.py
@@ -2,3 +2,5 @@ a,b = float(input()), float(input())
 sum1 = a+b
 avg = (a+b)/2
 print('sum:', sum1, 'avg:', avg)
+
+
