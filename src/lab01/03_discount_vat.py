8671ab6a59990f8e282cd7c4aeacefead51321ce
mdinara2008 <muhamedova2008@mail.ru>
2026-09-11 10:33:05 +0300

    1

--- src/lab01/03_discount_vat.py
+++ src/lab01/03_discount_vat.py
@@ -1,15 +1,11 @@
-price = float(input('price='))
-discount = float(input('discount='))
-vat = float(input('vat='))
+price = float(input('price=').replace(',','.'))
+discount = float(input('discount=').replace(',','.'))
+vat = float(input('vat=').replace(',','.'))
 
 base = price*(1-discount/100)
 vat_amount = base*(vat/100)
 total = base+vat_amount
 
-base1 = f'{base:.2f}'
-vat_amount1 = f'{vat_amount:.2f}'
-total1 = f'{total:.2f}'
-
-print('База после скидки: ', base1, '₽')
-print('НДС: ', vat_amount1, '₽')
-print('Итого к оплате: ', total1, '₽')
+print(f'База после скидки: {base:.2f}₽')
+print(f'НДС: {vat_amount:.2f}₽')
+print(f'Итого к оплате: {total:.2f}₽')
