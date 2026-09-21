3b06be86d2fa11d362ba259510cc1b2e4ebfa098
mdinara2008 <muhamedova2008@mail.ru>
2026-09-21 09:38:11 +0300

    1

--- src/lab01/06.py
+++ src/lab01/06.py
@@ -1,12 +1,12 @@
-N = int(input())
+n = int(input('in_1: '))
 ochno = 0
 zaochno = 0
-for i in range(N):
-    l_name, name, age, format = input().split()
+for i in range(n):
+    l_name, name, age, format = input(f'in_{i+2}: ').split()
     if format=='True':
         ochno+=1
     else:
         zaochno+=1
-print(f'{ochno} {zaochno}')
+print(f'out: {ochno} {zaochno}')
 
 
