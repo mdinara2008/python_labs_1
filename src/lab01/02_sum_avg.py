15331f5276568c06eeaa05b351bed0d71f9aec7a
mdinara2008 <muhamedova2008@mail.ru>
2026-09-21 09:24:06 +0300

    1

--- src/lab01/02_sum_avg.py
+++ src/lab01/02_sum_avg.py
@@ -1,6 +1,6 @@
 a,b = float(input('a=').replace(',','.')), float(input('b=').replace(',','.'))
 sum1 = a+b
 avg = (a+b)/2
-print(f'sum: {sum1:.2f}; avg: {avg:.2f}')
+print(f'sum={sum1:.2f}; avg={avg:.2f}')
 
 
